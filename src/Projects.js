7537316fd4470cceccff3c326b3eced22519e8bb
Baljit Singh <baljitsingh692002@gmail.com>
2024-04-20 23:16:38 +0530

    updated project

--- src/Projects.js
+++ src/Projects.js
@@ -40,11 +40,11 @@ function Project(props){
                 </div>
 
                 <div className='p-4 border-solid rounded-xl mt-2'>
-                  <h3 className='text-2xl'>Fine - connec </h3>
-                  <p className='p-2 mt-2'> Trying to create a E-commerce portal. this is backend part under development</p>
+                  <h3 className='text-2xl'>Fine - Finance  </h3>
+                  <p className='p-2 mt-2'> Trying to connect loan provider and loan seeker </p>
                    <div className='p-2 flex flex-row space-x-2 justify-center'>
-                    <button className='w-22 space-x-1 h-10 justify-center items-center flex p-2 rounded-lg bg-blue-600 text-gray-300'><a href="https://apnadaily-blog.netlify.app/" target="_blank">View Demo </a><LuExternalLink /></button>
-                    <button className='w-20 h-10 space-x-1 justify-center items-center flex  p-2 rounded-lg bg-blue-600 text-gray-300'><a href="https://github.com/TheBaljitSingh/Apna-Blog-Backend/" target="_blank">Github </a><LuExternalLink /></button>
+                    {/* <button className='w-22 space-x-1 h-10 justify-center items-center flex p-2 rounded-lg bg-blue-600 text-gray-300'><a href="/" target="_blank">View Demo </a><LuExternalLink /></button> */}
+                    <button className='w-20 h-10 space-x-1 justify-center items-center flex  p-2 rounded-lg bg-blue-600 text-gray-300'><a href="https://github.com/TheBaljitSingh/Gfg-hackathon-regionalRound-frontend" target="_blank">Github </a><LuExternalLink /></button>
                   </div>
                 </div>
                 </div>
